fe71d1b39a2e3cc5ec2d50674d61a26eb7d05833
Jonathan Prieto <jonathan.prieto@me.com>
2019-08-25 19:13:33 -0500

    Main -- El correcto pero no corre en la Uva Main2 -- Test inicial

--- main2.cpp
+++ main2.cpp
@@ -3,35 +3,28 @@
 #include <cstring>
 #include <vector>
 #include <algorithm>
-#include <map>
+
 using namespace std;
 
 int numeromujeres;
 string country;
 string nombre;
 string appellido;
-
-map<string, int> list_of_conquests;
+vector <string>paises;
+vector <string>vectorunic;
 
 void contarpaises(int numeromujeres){
     for (int i = 0; i < numeromujeres; i++) {
         cin>>country>>nombre>>appellido;
-        list_of_conquests[country] += 1;
+        paises.push_back(country);
     }
-}
-void imprimir(pair<string, int>  const& a)
-{
-
-    cout << a.first << "   " << a.second << endl;
+    vectorunic=unique(paises.begin(),paises.end());
 }
 
-int main()
-{
+
+
+
+int main() {
     cin>>numeromujeres;
     contarpaises(numeromujeres);
-    for_each (
-            list_of_conquests.begin(),
-            list_of_conquests.end(),
-            imprimir
-    );
 }
