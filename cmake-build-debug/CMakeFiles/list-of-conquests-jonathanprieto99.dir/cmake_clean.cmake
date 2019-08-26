49798ed7ca1a90e25ad82c1ec87993d9edd1fe61
Jonathan Prieto <jonathan.prieto@me.com>
2019-08-25 19:14:17 -0500

    First Upload

---
+++ cmake-build-debug/CMakeFiles/list-of-conquests-jonathanprieto99.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/list-of-conquests-jonathanprieto99.dir/main.cpp.o"
+  "list-of-conquests-jonathanprieto99.pdb"
+  "list-of-conquests-jonathanprieto99"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/list-of-conquests-jonathanprieto99.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
